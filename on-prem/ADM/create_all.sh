0bc73d593e047e5d98385e189de0b07c299a5e9a
Daniel Isaac <danielisaac@Daniels-MacBook-Pro.local>
2020-02-03 11:07:48 +0530

    config changes

--- on-prem/ADM/create_all.sh
+++ on-prem/ADM/create_all.sh
@@ -7,7 +7,8 @@ sed -i "s/\`ADM_AGENT_IP\`/$ADM_AGENT_IP/g" .temp/*.yaml
 sed -i "s/\`FINGERPRINT\`/$FINGERPRINT/g" .temp/*.yaml
 sed -i "s/\`NAMESPACE\`/$NAMESPACE/g" .temp/*.yaml
 kubectl create -f .temp/namespace.yaml 
-kubectl create -f .temp/team_hotdrink.yaml -n sg-demo
-kubectl create -f .temp/hotdrink-secret.yaml -n sg-demo
+kubectl create -f .temp/team_hotdrink.yaml -n $NAMESPACE
+kubectl create -f .temp/hotdrink-secret.yaml -n $NAMESPACE
 kubectl create -f .temp/rbac.yaml
-kubectl create -f .temp/cpx.yaml -n sg-demo
+kubectl create -f .temp/cpx.yaml -n $NAMESPACE
+kubectl create -f team-guestbook.yaml -n $NAMESPACE
